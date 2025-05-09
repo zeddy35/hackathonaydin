01e07915c70541e094b2920add1bb1b866f98250
Z. Erden Dereli <erden@Zs-MacBook-Air.local>
2025-05-09 22:19:10 +0300

    ilk yükleme

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`This server is running on port : ${port}`);
+});
